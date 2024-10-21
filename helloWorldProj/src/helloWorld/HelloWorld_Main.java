c2580ebbaeea89a17fcaef488a75ed9ba7499018
saikiran_greemus <saikiranjillella2894@gmail.com>
2024-10-21 12:58:53 +0530

    add the sop statement

--- helloWorldProj/src/helloWorld/HelloWorld_Main.java
+++ helloWorldProj/src/helloWorld/HelloWorld_Main.java
@@ -3,7 +3,8 @@ package helloWorld;
 public class HelloWorld_Main 
 {
 	public static void main(String[] args) {
-		System.out.println("Hello World....");
+		System.out.println("Hello World...."); 
+		System.out.println("Welcome to java world...");
 	}
 
 }
